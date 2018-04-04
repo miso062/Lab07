4f18793dbdcfcd0fd2fc7db35443a27243f9f663
miso062 <21600062@handong.edu>
2018-04-04 20:09:08 +0900

    calculates the century given a year.

--- src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
+++ src/main/java/edu/handong/csee/java/lab07/prob1/YearToCentury.java
@@ -1,35 +1,77 @@
 package edu.handong.csee.java.lab07.prob1;
 
+// Insert Scanner
 import java.util.Scanner;
+
+/**
+ * class YearToCentury
+ * get the year calculates the century given a year.
+ * 
+ * there is a one class variable
+ * and one method to calculate the century.
+ * 
+ * @author smile
+ *
+ */
 public class YearToCentury {
 	
-	int year;
+	// define class variable
+	int year; // year that will be calculated.
 	
+	/**
+	 * method YearToCen
+	 * get the year value and calculate the century given a year.
+	 * 
+	 * @param year
+	 * @return calculated century value.
+	 */
+	// method that calculate the century given a year
 	public int YearToCen (int year)
 	{
-		int century=1;
+		// insert integer variable century
+		int century;
 
-		if(year%100 == 0)
+		// if statement to check the year divided into 100.
+		if(year%100 == 0) // there is no remainder.
+			// put value that divide the year by 100 into century
 			century = year/100;
-		else
+		else // there is remainder.
+			// put value that divide the year by 100 and add 1 into century
 			century = year/100 + 1;
-			
+		
+		// return century
 		return century;
 	}
 
+	/**
+	 * main program
+	 * 
+	 * get year from user,
+	 * calculate the century given the year,
+	 * and print out the century.
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
-		int year;
-		int century;
+		// define integer variable
+		int year; // year that user want to calculate.
+		int century; // calculated century value.
 		
+		// Instantiate an object of class YearToCentury
 		YearToCentury Year = new YearToCentury();
+		// Create an object of the Scanner
 		Scanner keyboard = new Scanner(System.in);
 		
+		// Use the println to get the year from user.
 		System.out.println("Input year: ");
+		// save the entered number as year.
 		year = keyboard.nextInt();
 		
+		// calculate the century by calling the YearToCen method and put the value into century.
 		century = Year.YearToCen(year);
 		
+		// print out the year and the century.
 		System.out.println(year + " is " + century + "th century.");
 		
 	}
